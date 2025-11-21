2f1ce5e1752661010ee28afd1946cf7cfdf3c33e
Francisco Meza Dastres <franciscomeza.1234@gmail.com>
2025-11-21 14:53:09 +0000

    feat: :zap: adding news python files

---
+++ hola.py
@@ -0,0 +1,6 @@
+#su nombre
+print("Francisco Meza Dastres")
+#su cuidad 
+print("Soy de la ciudad de Santiago")
+#emoticon
+print("😊")
---
+++ numeros.py
@@ -0,0 +1,10 @@
+entero = 12
+decimal = 1.12
+suma = entero + 5 
+modulo = entero % 2
+
+print ("La suma es", suma)
+print ("El valor del decimal es", decimal)
+print ("Division:", ((entero  / 3 )+ 2) ) 
+print ("Potencia:", (entero **2))
+print ("Modulo:", modulo)
