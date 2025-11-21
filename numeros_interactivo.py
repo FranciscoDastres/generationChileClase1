8c4f4f6dd20652719760c7e98fac1b53793fdfe1
Francisco Meza Dastres <franciscomeza.1234@gmail.com>
2025-11-21 15:46:52 +0000

    feat: :zap: adding new file numeros_interactivo.py

---
+++ numeros_interactivo.py
@@ -0,0 +1,6 @@
+numero1 = float( input("Ingresa el primer número:"))
+numero2 = float( input("Ingresa el segundo número"))
+
+print (type(numero1))
+print (type(numero2))
+print(f"La suma de {numero1}y {numero2} es {numero1 + numero2}")
